8e6da740c5172cb854dfafc43db17c5069ce87a3
Israel Alexsander <rael3014@gmail.com>
2023-04-14 12:13:10 -0300

    Updates

---
+++ precache-manifest.41b9a6373614200dbe9122e8b167de78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30f7d3695b035e8824a",
+    "url": "/Calculadora-React/static/css/main.1759cf40.chunk.css"
+  },
+  {
+    "revision": "c30f7d3695b035e8824a",
+    "url": "/Calculadora-React/static/js/main.c30f7d36.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/Calculadora-React/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "f75127523cc5a078398c",
+    "url": "/Calculadora-React/static/js/2.f7512752.chunk.js"
+  },
+  {
+    "revision": "a3227ede79d05a1cfb96",
+    "url": "/Calculadora-React/static/js/runtime~main.a3227ede.js"
+  },
+  {
+    "revision": "d1a7b45f28bf337cab8adf3992f669a0",
+    "url": "/Calculadora-React/static/media/RobotoMono-Thin.d1a7b45f.ttf"
+  },
+  {
+    "revision": "eb7778462e52069652373e6ad604b1f1",
+    "url": "/Calculadora-React/index.html"
+  }
+];
